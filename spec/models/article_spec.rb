0a84629d0c2910ea41d0ced149d675971241c44d
chamakov <jfoa92@hotmail.com>
2018-12-11 14:52:01 -0600

    Crea el modelo action

--- spec/models/article_spec.rb
+++ spec/models/article_spec.rb
@@ -5,7 +5,7 @@ RSpec.describe Article, type: :model do
   # Espera que el atributo status este presente
   it { is_expected.to validate_presence_of(:status) }
   # Espera que el atributo status tenga la enumeracion correcta
-  xit { is_expected.to define_enum_for(:status).with(inactive: 0, active: 1) }
+  it { is_expected.to define_enum_for(:status).with(inactive: 0, active: 1) }
   # Espera que el atributo slug este presente
   it { is_expected.to validate_presence_of(:slug) }
   # Espera que el atributo name este presente
